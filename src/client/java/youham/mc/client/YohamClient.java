047d3c6994f401c2901e8ceeeddadc23ecfb5d28
Matt W <mattwip.git@gmail.com>
2026-04-04 15:05:00 -0500

    Initial commit of YoHam Minecraft plugin. Files created from Fabric template mod generator (https://fabricmc.net/develop/template/).

---
+++ src/client/java/youham/mc/client/YohamClient.java
@@ -0,0 +1,10 @@
+package youham.mc.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class YohamClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
